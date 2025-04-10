2b3bb10f4b6114d87e177ff7962c795958d96a09
Tirth S Pandya <teamfznltd@gmail.com>
2025-04-10 19:34:23 +0530

    .

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+// write a program to demonstrate simple C++ structure with help of cin and cout
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    cout<<"Enter your name"<<endl;
+    cin>>name;
+    cout<<"Hello there! "<<name<<endl;
+
+    return 0;
+}
